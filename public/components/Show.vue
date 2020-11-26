e9121ea5477033f0093afa7cbfb892fac970cb00
Mirko Westermeier <mirko@westermeier.de>
2020-11-26 21:39:15 +0100

    Add an edit button to content page (closes #2)

--- public/components/Show.vue
+++ public/components/Show.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="show">
         <div id="markdown" v-html="html"></div>
+        <div id="buttons">
+            <router-link :to="{name: 'Edit'}" title="Edit">
+                <i class="fas fa-edit"></i>
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -32,7 +37,7 @@ module.exports = {
     background: #f0f0f0;
     max-height: 90%;
     overflow-y: auto;
-    margin: 3em 0;
+    margin: 3em 0 1em;
     padding: .5em 2em;
     line-height: 1.5em;
     font-size: 1.2rem;
@@ -40,8 +45,23 @@ module.exports = {
 }
 @media (max-width: 60rem) {
     #show > #markdown {
-        margin: 1em 0 !important;
+        margin: 1em 0 1em !important;
         padding: 0 1em !important;
     }
+}
+
+#show > #buttons {
+    flex: 0 0;
+    margin-bottom: 3em;
+}
+@media (max-width: 60rem) { #show > #buttons {margin-bottom: 1em !important} }
+
+#show > #buttons > a {
+    color: lightgrey;
+    text-decoration: none;
+}
+#show > #buttons > a:hover,
+#show > #buttons > a:active {
+    color: darkgrey;
 }
 </style>
