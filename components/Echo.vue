8f18acc8920359633a545654fd764a1d8b6e602e
Mirko Westermeier <mirko@westermeier.de>
2020-12-29 19:48:29 +0100

    Set default font to Noto Sans

--- components/Echo.vue
+++ components/Echo.vue
@@ -65,6 +65,8 @@ module.exports = {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+* {font-family: 'Noto Sans', sans-serif}
 html, body {margin: 0; padding: 0}
 h1, h2, h3, h4, h5, h6 {line-height: 1.2em}
 </style>
